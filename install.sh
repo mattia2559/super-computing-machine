e56127813be91c94cede7d4953d983a68bf16b81
mattia2559 <135450022+mattia2559@users.noreply.github.com>
2024-04-09 17:44:00 +0200

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,7 @@
+apt install qemu bash coreutils ovmf grep jq lsb-base procps python3 genisoimage usbutils util-linux sed spice-client-gtk libtss2-tcti-swtpm0 wget xdg-user-dirs zsync unzip
+sudo add-apt-repository ppa:yannick-mauray/quickgui
+sudo apt update
+sudo apt install quickgui
+sudo apt-add-repository ppa:flexiondotorg/quickemu
+sudo apt update
+sudo apt install quickemu
